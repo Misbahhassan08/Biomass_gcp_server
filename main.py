fa0b6d3a8a62872aaee9dc9db2ed5e4e072b7fc5
misbahhassan08 <misbahhassan08@gmail.com>
2023-07-03 19:06:26 +0500

    add load csv and graphs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from time import sleep
+from io import StringIO
 import datetime
 from flask import Flask, jsonify, send_file, redirect, url_for, request
 from flask_cors import CORS, cross_origin
@@ -15,6 +16,8 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
 CORS(app)
 app.app_context().push()
+# Define allowed files
+ALLOWED_EXTENSIONS = {'csv'}
 
 class MyDataBase:
     def __init__(self):
@@ -42,39 +45,84 @@ class MyDataBase:
          self.cursor.execute(f""" SELECT * from {tableName} WHERE GroupID={GroupID}""")
          self.cursor.connection.commit()
          output = self.cursor.fetchall()
-         #print(f"data:{output}")
-         #for i in output:
-         #   print(f" Data is = {i}")
          self.cursor.close() #Closing the cursor
          return output # end of fetchTableData function
-    def fetchSelectedData(self, tableName,Data_Point):
+    def fetchSelectedData(self, tableName,Data_Point,CsvfileID):
          self.cursor = self.connection.cursor()
-         self.cursor.execute(f""" SELECT * from {tableName} WHERE Data_Point = {Data_Point}""")
+         self.cursor.execute(f""" SELECT * from {tableName} WHERE (Data_Point = {Data_Point}) AND (CsvfileID = {CsvfileID})""")
          self.cursor.connection.commit()
          output = self.cursor.fetchall()
-         #print(f"data:{output}")
-         #for i in output:
-         #   print(f" Data is = {i}")
          self.cursor.close() #Closing the cursor
          return output # end of fetchSelectedData function
 
     def insertToDB(self, query, values, table_name):
-        metadata = MetaData()
         self.cursor = self.connection.cursor()
         self.cursor.execute(query, values)
         self.cursor.connection.commit()
-
         self.cursor.execute(f""" SELECT * from {table_name}""")
         output = self.cursor.fetchall()
-        print(f"data:{output}")
-        #row = 0
-        #for i in output:
-            #data = i[metadata.CsvfileID]
-            #row = data
-        #Closing the cursor
-        self.cursor.close()
+        
+        data = None
+        for i in output:
+            data = i
+        print(f"In {table_name} Last Saved Data is :{data}")
+        self.cursor.close() #Closing the cursor
+        return data # end of insertToDB function
 
-        #return row # end of insertToDB function
+@app.route("/api/load_csv_meta_data" , methods=['POST'])
+def load_csv_meta_data():
+    db = MyDataBase()
+    metadata = MetaData()
+    csvtbl = "csvtbl"
+    rpi_id = 1
+
+    if request.method == 'POST':
+        CsvfileDirectory = request.form.get('CsvfileDirectory')
+        print(f"Coming csv file directory name : {CsvfileDirectory}")
+        file = request.files['file']
+        print(f"Coming csv file name : {file.filename}")
+        if file.mimetype != 'text/csv':
+            return 'Only CSV files are allowed!', 400
+        if file.filename == '':
+            return 'No file selected!', 400
+        csvData = file.read().decode('utf-8')
+        df = pd.read_csv(StringIO(csvData)) # String to buffer and load in pandas data frame type
+        dictData = df.to_dict(orient='record')
+        query  =  (f"""INSERT INTO {csvtbl} (RPIID, CsvfileDirectory)  VALUES (%s, %s)""")
+        values = (rpi_id,CsvfileDirectory)
+        lastRow = db.insertToDB(query, values,csvtbl)
+        CsvfileID = lastRow[metadata.CsvfileID]
+        
+        for x in dictData:
+            print(f'[INFO] Cal_Avg_Red_650nm data is :{x[metadata.Cal_Avg_Red_650nm]}')
+            metadatatbl = "metadatatbl"
+            query = ("""INSERT INTO metadatatbl (CsvfileID, Data_Point, Sample_Num,Time_Stamp, Time_Per, Temp, Gain, Int_Time, Allowable_Dev, 
+                                    Raw_Used_Vio, Raw_Values_Vio_450nm, Raw_Selected_Vio_450nm, Raw_Avg_Vio_450nm, Raw_StdDev_Vio, Cal_Used_Vio, Cal_Values_Vio_450nm, Cal_Selected_Vio_450nm, Cal_Avg_Vio_450nm, Cal_StdDev_Vio,
+                                    Raw_Used_Blu, Raw_Values_Blu_500nm, Raw_Selected_Blu_500nm, Raw_Avg_Blu_500nm, Raw_StdDev_Blu, Cal_Used_Blu, Cal_Values_Blu_500nm, Cal_Selected_Blu_500nm, Cal_Avg_Blu_500nm, Cal_StdDev_Blu,
+                                    Raw_Used_Grn, Raw_Values_Grn_550nm, Raw_Selected_Grn_550nm, Raw_Avg_Grn_550nm, Raw_StdDev_Grn, Cal_Used_Grn, Cal_Values_Grn_550nm, Cal_Selected_Grn_550nm, Cal_Avg_Grn_550nm, Cal_StdDev_Grn,
+                                    Raw_Used_Yel, Raw_Values_Yel_570nm, Raw_Selected_Yel_570nm, Raw_Avg_Yel_570nm, Raw_StdDev_Yel, Cal_Used_Yel, Cal_Values_Yel_570nm, Cal_Selected_Yel_570nm, Cal_Avg_Yel_570nm, Cal_StdDev_Yel,
+                                    Raw_Used_Org, Raw_Values_Org_600nm, Raw_Selected_Org_600nm, Raw_Avg_Org_600nm, Raw_StdDev_Org, Cal_Used_Org, Cal_Values_Org_600nm, Cal_Selected_Org_600nm, Cal_Avg_Org_600nm, Cal_StdDev_Org,
+                                    Raw_Used_Red, Raw_Values_Red_650nm, Raw_Selected_Red_650nm, Raw_Avg_Red_650nm, Raw_StdDev_Red, Cal_Used_Red, Cal_Values_Red_650nm, Cal_Selected_Red_650nm, Cal_Avg_Red_650nm, Cal_StdDev_Red) VALUES
+                                    (%s, %s, %s, %s,%s, %s, %s, %s, %s, 
+                                    %s, %s, %s, %s, %s, %s, %s,%s, %s, %s,
+                                    %s, %s, %s, %s, %s, %s, %s,%s, %s, %s,
+                                    %s, %s, %s, %s, %s, %s, %s,%s, %s, %s , 
+                                    %s, %s, %s, %s, %s, %s, %s,%s, %s, %s,
+                                    %s, %s, %s, %s, %s, %s, %s,%s, %s, %s,
+                                    %s, %s, %s, %s, %s, %s, %s,%s, %s, %s   
+                                    ) """)
+            values = (CsvfileID, x[metadata.Data_Point], x[metadata.Sample_Num], x[metadata.Time_Stamp] , x[metadata.Time_Per] , x[metadata.Temp], x[metadata.Gain], x[metadata.Int_Time], x[metadata.Allowable_Dev],
+                                x[metadata.Raw_Used_Vio],x[metadata.Raw_Values_Vio_450nm] , x[metadata.Raw_Selected_Vio_450nm] , x[metadata.Raw_Avg_Vio_450nm] , x[metadata.Raw_StdDev_Vio] , x[metadata.Cal_Used_Vio] , x[metadata.Cal_Values_Vio_450nm], x[metadata.Cal_Selected_Vio_450nm], x[metadata.Cal_Avg_Vio_450nm], x[metadata.Cal_StdDev_Vio], 
+                                x[metadata.Raw_Used_Blu],x[metadata.Raw_Values_Blu_500nm] , x[metadata.Raw_Selected_Blu_500nm] , x[metadata.Raw_Avg_Blu_500nm] , x[metadata.Raw_StdDev_Blu] , x[metadata.Cal_Used_Blu] , x[metadata.Cal_Values_Blu_500nm], x[metadata.Cal_Selected_Blu_500nm], x[metadata.Cal_Avg_Blu_500nm], x[metadata.Cal_StdDev_Blu],  
+                                x[metadata.Raw_Used_Grn],x[metadata.Raw_Values_Grn_550nm] , x[metadata.Raw_Selected_Grn_550nm] , x[metadata.Raw_Avg_Grn_550nm] , x[metadata.Raw_StdDev_Grn] , x[metadata.Cal_Used_Grn] , x[metadata.Cal_Values_Grn_550nm], x[metadata.Cal_Selected_Grn_550nm], x[metadata.Cal_Avg_Grn_550nm], x[metadata.Cal_StdDev_Grn],  
+                                x[metadata.Raw_Used_Yel],x[metadata.Raw_Values_Yel_570nm] , x[metadata.Raw_Selected_Yel_570nm] , x[metadata.Raw_Avg_Yel_570nm] , x[metadata.Raw_StdDev_Yel] , x[metadata.Cal_Used_Yel] , x[metadata.Cal_Values_Yel_570nm], x[metadata.Cal_Selected_Yel_570nm], x[metadata.Cal_Avg_Yel_570nm], x[metadata.Cal_StdDev_Yel], 
+                                x[metadata.Raw_Used_Org],x[metadata.Raw_Values_Org_600nm] , x[metadata.Raw_Selected_Org_600nm] , x[metadata.Raw_Avg_Org_600nm] , x[metadata.Raw_StdDev_Org] , x[metadata.Cal_Used_Org] , x[metadata.Cal_Values_Org_600nm], x[metadata.Cal_Selected_Org_600nm], x[metadata.Cal_Avg_Org_600nm], x[metadata.Cal_StdDev_Org], 
+                                x[metadata.Raw_Used_Red],x[metadata.Raw_Values_Red_650nm] , x[metadata.Raw_Selected_Red_650nm] , x[metadata.Raw_Avg_Red_650nm] , x[metadata.Raw_StdDev_Red] , x[metadata.Cal_Used_Red] , x[metadata.Cal_Values_Red_650nm], x[metadata.Cal_Selected_Red_650nm], x[metadata.Cal_Avg_Red_650nm], x[metadata.Cal_StdDev_Red]
+                                )
+
+            db.insertToDB(query, values, metadatatbl)
+    return jsonify({"response": False,"result": "Success"})
+
 @app.route("/api/add_group" , methods=['POST'])
 def add_group():
     db = MyDataBase()
@@ -155,11 +203,14 @@ def get_graph_meta_data():
         return jsonify({"response": False,"result": "Please Send Post request here!"})
     elif request.method == 'POST':
         postJson = request.get_json()
+        CsvfileID = postJson["CsvfileID"]
+        
         for Data_Point in range(0,len(postJson["Data_Point"])):
             data_point = postJson["Data_Point"][Data_Point]
             print(data_point)
-            mainData = db.fetchSelectedData("metadatatbl",data_point)
+            mainData = db.fetchSelectedData("metadatatbl",data_point,CsvfileID)
             rawDataPoint = None
+            print(mainData)
             for x in mainData:
                 row = {"RPI_DataID":x["RPI_DataID"], "CsvfileID":x["CsvfileID"],metadata.Data_Point:x[metadata.Data_Point],metadata.Sample_Num:x[metadata.Sample_Num], metadata.Time_Stamp:str(x[metadata.Time_Stamp]) , metadata.Time_Per:x[metadata.Time_Per] , metadata.Temp:x[metadata.Temp], metadata.Gain:x[metadata.Gain], metadata.Int_Time:x[metadata.Int_Time], metadata.Allowable_Dev:x[metadata.Allowable_Dev],
                                     metadata.Raw_Used_Vio:x[metadata.Raw_Used_Vio],metadata.Raw_Values_Vio_450nm:x[metadata.Raw_Values_Vio_450nm] , metadata.Raw_Selected_Vio_450nm:x[metadata.Raw_Selected_Vio_450nm] , metadata.Raw_Avg_Vio_450nm:x[metadata.Raw_Avg_Vio_450nm] , metadata.Raw_StdDev_Vio:x[metadata.Raw_StdDev_Vio] , metadata.Cal_Used_Vio:x[metadata.Cal_Used_Vio] , metadata.Cal_Values_Vio_450nm:x[metadata.Cal_Values_Vio_450nm], metadata.Cal_Selected_Vio_450nm:x[metadata.Cal_Selected_Vio_450nm], metadata.Cal_Avg_Vio_450nm:x[metadata.Cal_Avg_Vio_450nm], metadata.Cal_StdDev_Vio:x[metadata.Cal_StdDev_Vio], 
@@ -170,11 +221,13 @@ def get_graph_meta_data():
                                     metadata.Raw_Used_Red:x[metadata.Raw_Used_Red],metadata.Raw_Values_Red_650nm:x[metadata.Raw_Values_Red_650nm] , metadata.Raw_Selected_Red_650nm:x[metadata.Raw_Selected_Red_650nm] , metadata.Raw_Avg_Red_650nm:x[metadata.Raw_Avg_Red_650nm] , metadata.Raw_StdDev_Red:x[metadata.Raw_StdDev_Red] , metadata.Cal_Used_Red:x[metadata.Cal_Used_Red] , metadata.Cal_Values_Red_650nm:x[metadata.Cal_Values_Red_650nm], metadata.Cal_Selected_Red_650nm:x[metadata.Cal_Selected_Red_650nm], metadata.Cal_Avg_Red_650nm:x[metadata.Cal_Avg_Red_650nm], metadata.Cal_StdDev_Red:x[metadata.Cal_StdDev_Red]
                 }
                 data.append(row)
+                print(f"[INFO] Length of Rows : {len(row)}")
                 rawDataPoint = x[metadata.Data_Point]
             singleDataPointData = {"Data_Point": rawDataPoint,"Samples": data}
             totalData.append(singleDataPointData)
+            print(f"[INFO] Sending Data Length is : {len(data)}")
             data = []
-    print(totalData)
+    #print(totalData)
     return jsonify({"response": False,"result": totalData})
 
 
